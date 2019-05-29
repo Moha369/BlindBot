89644cdaa1ef5a53ef8b9446e914b14afb3b2810
Moha369 <mshaawa963@gmail.com>
2019-05-29 14:32:15 +0300

    Delete testing.py

--- src/testing.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def get_id(url):
-    iden = re.findall('/lichess4545.slack.com/messages/([A-Z0-9])/$', url)
-    print(iden)
-
-get_id('https://lichess4545.slack.com/messages/G0DFRURGQ/details/')
